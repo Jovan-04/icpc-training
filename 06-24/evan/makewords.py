0f0c4d0ea17e1a77cf878249bcaa8e0bf659a505
Jovan-04 <evanne.scherrer@gmail.com>
2024-07-02 19:55:22 -0500

    update big test cases

--- 06-24/evan/makewords.py
+++ 06-24/evan/makewords.py
@@ -3,9 +3,9 @@ import json
 with open('./words_dictionary.json') as input:
   words: dict[str, int] = json.load(input)
 
-with open('./bigtestcase.txt', 'w') as output:
+with open('./bigtestcase7.txt', 'w') as output:
   for word in words.keys():
-    if len(word) >= 16:
-      output.write(word[:16] + '\n')
+    if len(word) == 7:
+      output.write(word[:7] + '\n')
   output.close()
 
